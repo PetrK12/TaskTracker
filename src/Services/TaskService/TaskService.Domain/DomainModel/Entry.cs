7d9852df3181c5b9978ab858ec728b2eaaae290b
Petrk12 <petrkutil123@gmail.com>
2023-05-22 13:43:48 +0200

    Project initial implementation + TaskService domain layer implementation

---
+++ src/Services/TaskService/TaskService.Domain/DomainModel/Entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TaskService.Domain.DomainModel
+{
+	public class Entry
+	{
+		public string Name;
+		public string Description;
+		public int Id;
+		public DateTime DateCreated;
+		public DateTime DueDate;
+		public bool IsDone;
+		public bool IsOverdue;
+		public string Owner;
+		public IEnumerable<string> Colaborators;
+	}
+}
+
---
+++ src/Services/TaskService/TaskService.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TaskService.Domain.DomainModel;
+
+namespace TaskService.Domain.Interfaces
+{
+	public interface ITaskRepository
+	{
+		public Task<bool> CreateTask(Entry task);
+
+		public Task<bool> UpdateTask(Entry task);
+
+		public Task<bool> DeleteTask(Entry task);
+
+		public Task<IEnumerable<Entry>> ListTasks();
+
+		public Task<Entry> GetEntry(int id);
+	}
+}
+
