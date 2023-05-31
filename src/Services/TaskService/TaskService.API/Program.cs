11f8db47409779225da9dd2bb3ef1aac3f4e348d
Petrk12 <petrkutil123@gmail.com>
2023-05-31 20:12:57 +0200

    Add swagger with token

--- src/Services/TaskService/TaskService.API/Program.cs
+++ src/Services/TaskService/TaskService.API/Program.cs
@@ -2,6 +2,7 @@
 using TaskService.Application.Extensions;
 using TaskService.API;
 using System.Reflection;
+using Microsoft.OpenApi.Models;
 using TaskService.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,39 @@ builder.Services.AddAuthentication();
 builder.Services.AddAutoMapper(cfg => cfg.AddProfile(typeof(MappingProfiles)));
 builder.Services.ConfigureJwt(builder.Configuration);
 builder.Services.AddScoped<IAuthManager, AuthManager>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer schema.
+                        Enter 'Bearer' [space] and then your token in the text input bellow
+                        Example: 'Bearer 12234asfk'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{ 
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            },
+            Scheme = "Oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header
+        },
+        new List<string>()
+    }
+    });
+    
+    c.SwaggerDoc("v1", new OpenApiInfo{Title = "TaskService.API", Version = "v1"});
+});
+
 
 var app = builder.Build();
 
@@ -27,6 +61,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 //app.MigrateDatabase();
 app.UseAuthorization();
