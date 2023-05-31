03c0134bd3e2a4c8993cfa790aa12842b27f82a3
Petrk12 <petrkutil123@gmail.com>
2023-05-31 18:58:08 +0200

    user roles

--- src/Services/TaskService/TaskService.API/Migrations/20230531165743_AddedUserRoles.Designer.cs
+++ src/Services/TaskService/TaskService.API/Migrations/20230531165743_AddedUserRoles.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskService.Infrastructure.AppDbContext;
 
@@ -11,9 +12,11 @@ using TaskService.Infrastructure.AppDbContext;
 namespace TaskService.API.Migrations
 {
     [DbContext(typeof(EntryContext))]
-    partial class EntryContextModelSnapshot : ModelSnapshot
+    [Migration("20230531165743_AddedUserRoles")]
+    partial class AddedUserRoles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Services/TaskService/TaskService.API/Migrations/20230531165743_AddedUserRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaskService.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2328b12a-acfe-4451-99d9-16dc23f687de", null, "User", "USER" },
+                    { "df51feeb-c567-4fae-8c95-bfc064f7f648", null, "Administrator", "ADMINISTRATOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2328b12a-acfe-4451-99d9-16dc23f687de");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "df51feeb-c567-4fae-8c95-bfc064f7f648");
+        }
+    }
+}
